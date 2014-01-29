5a212cf7411a019df5ed7a1a2c56b979eba6a3f8
Cross <cross@djondb.com>
2014-01-29 10:22:30 +0000

    README and test.js added

--- test.js
+++ test.js
@@ -10,14 +10,15 @@ con.open();
 con.dropNamespace("test_nodejs", "nodens");
 con.insert("test_nodejs", "nodens", { name: "Test" });
 
-var res = con.find("test_nodejs", "nodens");
+var cursor = con.find("test_nodejs", "nodens");
 
-console.log(res);
+while (cursor.next()) {
+	var res = cursor.current();
 
-var guid = res[0]._id;
-var res2 = con.findByKey("test_nodejs", "nodens", guid);
+	var guid = res._id;
+	var res2 = con.findByKey("test_nodejs", "nodens", guid);
 
-console.log("res2");
-console.log(res2);
-
+	console.log("res2");
+	console.log(res2);
+}
 manager.releaseConnection(con);
